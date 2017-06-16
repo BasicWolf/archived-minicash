cce7b9b63a482169ef89818efe1c2bff1fcf2054
Zaur Nasibov <zaur@znasibov.info>
2017-06-16 22:59:25 +0300

    Add in-memory database to testing settings

--- src/minicash/app/settings/test.py
+++ src/minicash/app/settings/test.py
@@ -1,5 +1,11 @@
 from .dev import *
 
+
+DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+}
+
 # Tests speedup
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
