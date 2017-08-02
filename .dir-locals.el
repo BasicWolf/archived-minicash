8967605c57d2d509b8f3dfdd6f40138e22df6c55
Zaur Nasibov <zaur@znasibov.info>
2017-08-02 22:01:45 +0300

    Ignore *.elc files

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,2 @@
 ((python-mode . ((eval . (message "Setting up Minicash environment for Emacs"))
-                 (eval . (pyvenv-workon "minicash"))
                  (eval . (flycheck-set-checker-executable 'python-flake8 (expand-file-name "~/.virtualenvs/minicash/bin/flake8"))))))
